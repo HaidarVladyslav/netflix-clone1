5fe394353809cea1a2f6190ca1108f513932d925
HaidarVladyslav <vladyslavhaidar@gmail.com>
2023-12-24 15:58:50 +0200

    commented out guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./home/home.component'), canActivate: [authGuard] },
+  { path: '', loadComponent: () => import('./home/home.component'), canActivate: [] }, // authGuard
   { path: 'auth/login', loadComponent: () => import('./auth/login/login.component') },
   { path: 'auth/register', loadComponent: () => import('./auth/register/register.component') },
-  { path: 'account', loadComponent: () => import('./account/account.component'), canActivate: [authGuard] }
+  { path: 'account', loadComponent: () => import('./account/account.component'), canActivate: [] } // authGuard
 ];
